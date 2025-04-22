002030fa70fe230f4e7371519e41a09564064033
Axror <abdulmamatov@gamil.com>
2025-04-22 20:20:33 +0500

    yangilanish

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, about_us
 
 urlpatterns = [
     path("", home, name="home"),
+
+    path("about_us/", about_us, name="about_us"),
 ]
