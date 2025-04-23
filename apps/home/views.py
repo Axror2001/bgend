e5226f059845ecd45c8fbca1482ce77abf0ba609
Axror <abdulmamatov@gamil.com>
2025-04-23 18:21:23 +0500

    yangilanish

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home/index.html")
+    current_url='asosiy'
+
+    context = {'current_url': current_url}
+    return render(request, "home/index.html",context )
 
 def about_us(request):
-    return render (request, "home/about_us.html")
+    current_url='biz-haqimizda'
+
+    context = {'current_url': current_url}
+    return render (request, "home/about_us.html", context)
