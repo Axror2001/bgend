7e671287637f12e7f6af602e9de453c984ed6e93
Axror <abdulmamatov@gamil.com>
2025-04-23 20:00:03 +0500

    Kamchiliklar toldirilvotti

--- apps/home/models.py
+++ apps/home/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class AboutUs(models.Model):
     our_history1 = models.TextField(blank=True, null=True)
     our_history2 = models.TextField(blank=True, null=True)
+    our_history3 = models.TextField(blank=True, null=True)
+    our_history4 = models.TextField(blank=True, null=True)
+    our_history5 = models.TextField(blank=True, null=True)
+    our_history6 = models.TextField(blank=True, null=True)
 
 
     def __str__(self):
